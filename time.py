448d0be1bd198c2ef7b5e1b9ef85cbc09da4d36a
gunnar pope <gunnar@gmail.com>
2018-09-30 21:56:42 -0400

    create string for timestamp

--- time.py
+++ time.py
@@ -1,10 +1,15 @@
 from datetime import datetime, date
 
+def getNowTime():
+    return ( time.__format__("%Y-%m-%dT%H-%M-%S") )
+
+
 if __name__ == '__main__':
     time = datetime.now()
     date = date.today()
 
-    print(time)
+    nowtime = getNowTime()
+    print( nowtime )
     print(date)
     print(time.year)
     print(time.day)
@@ -13,5 +18,12 @@ if __name__ == '__main__':
     print(time.minute)
     print(time.second)
     print(time.microsecond)
+
+    iso = date.isocalendar()
+    print("ISO year: ", iso[0])
+    print("ISO weeknumber: ", iso[1])
+    print("ISO day: ", iso[2], "(1 = Monday)")
+
+
 
 
