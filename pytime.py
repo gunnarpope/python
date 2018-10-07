95f4bfd5ada8fb79556cee4892dd37b93990634a
gunnar pope <gunnar@gmail.com>
2018-10-07 19:51:37 -0400

    adding function calls for all real-time parameters

--- pytime.py
+++ pytime.py
@@ -1,29 +1,66 @@
 from datetime import datetime, date
 
 def getNowTime():
+    time = datetime.now()
     return ( time.__format__("%Y-%m-%dT%H-%M-%S") )
+
+def getNowTimeMicroSec():
+    time = datetime.now()
+    return ( time.__format__("%Y-%m-%dT%H-%M-%S.%f") )
+
+def getDOW():
+    """Returns Day of the Week where Sunday=0"""
+    d = date.today()
+    iso = d.isocalendar()
+    DOW = iso[2]
+
+    if (DOW == 7):
+        DOW = 0
+
+    return DOW
+
+def getYear():
+    time = datetime.now()
+    return time.year
+
+def getMonth():
+    time = datetime.now()
+    return time.month
+
+def getDay():
+    time = datetime.now()
+    return time.day
+
+def getHour():
+    time = datetime.now()
+    return time.hour
+
+def getMinute():
+    time = datetime.now()
+    return time.minute
+
+def getSecond():
+    time = datetime.now()
+    return time.second
+
+def getMicrosecond():
+    time = datetime.now()
+    return time.microsecond
+
+
+
 
 
 if __name__ == '__main__':
-    time = datetime.now()
-    date = date.today()
 
-    nowtime = getNowTime()
-    print( nowtime )
-    print(date)
-    print(time.year)
-    print(time.day)
-    print(time.month)
-    print(time.hour)
-    print(time.minute)
-    print(time.second)
-    print(time.microsecond)
-
-    iso = date.isocalendar()
-    print("ISO year: ", iso[0])
-    print("ISO weeknumber: ", iso[1])
-    print("ISO day: ", iso[2], "(1 = Monday)")
-
-
+    print( getYear())
+    print( getMonth())
+    print( getDay())
+    print( getDOW())
+    print( getHour())
+    print( getMinute())
+    print( getSecond())
+    print( getNowTimeMicroSec())
+    print( getNowTime())
 
 
