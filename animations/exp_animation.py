3931aa26a6e5fa8361bac204bd6a81c80bfd3459
gunnar pope <gunnar@gmail.com>
2019-04-30 14:03:24 -0400

    adding two plots

--- animations/exp_animation.py
+++ animations/exp_animation.py
@@ -10,10 +10,14 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
-xdata, ydata = [], []
+xdata, ydata, zdata = [], [], []
 # ln, = plt.plot([], [], 'ro')
-ln, = plt.plot([], [], 'r', mfc='none')
-plt.xlabel('Time (s)')
+ln, = plt.plot([], [], 'ro', mfc='none')
+# ln, = plt.plot([[]], [[]], 'r', mfc='none')
+plt.title('The Voltage, $V_c(t)$, over Time, t')
+plt.xlabel('Time, t (sec)')
+plt.ylabel('$V_c(t)$')
+plt.grid('True')
 
 
 Vcc  = 3
@@ -29,16 +33,21 @@ def init():
     return ln,
 
 def yfunk(t):
-    return Vcc*(1-np.exp(-t))
+
+    return ( Vcc*(1-np.exp(-t)), Vcc*(np.exp(-t))) 
 
 def update(frame):
+    y, z = yfunk(frame)
     xdata.append(frame)
-    ydata.append(yfunk(frame))
-    ln.set_data(xdata, ydata)
-    return ln,
+    ydata.append(y)
+    zdata.append(z)
+    ln.set_data([xdata,xdata],[ydata,zdata])        
+    # ydata.append(yfunk(frame))
+    # ln.set_data(xdata, ydata)
+    return ln
 
 ani = animation.FuncAnimation(fig, update, frames=np.linspace(0, tmax, 25),
-                    init_func=init,interval=50, repeat=False,blit=False)
+                    init_func=init,interval=20, repeat=False,blit=False)
 # repeat_delay=1000,
 
 # To save the animation, use e.g.
