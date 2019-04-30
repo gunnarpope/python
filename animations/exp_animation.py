1fa6313cbaf0a45ee2aa5066cdf49747b704bc64
gunnar pope <gunnar@gmail.com>
2019-04-30 14:51:41 -0400

    making mp4 works

--- animations/exp_animation.py
+++ animations/exp_animation.py
@@ -11,8 +11,7 @@ import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
 xdata, ydata, zdata = [], [], []
-# ln, = plt.plot([], [], 'ro')
-ln, = plt.plot([], [], 'ro', mfc='none')
+ln, = plt.plot([], [], 'r.', markersize=3)
 # ln, = plt.plot([[]], [[]], 'r', mfc='none')
 plt.title('The Voltage, $V_c(t)$, over Time, t')
 plt.xlabel('Time, t (sec)')
@@ -41,23 +40,20 @@ def update(frame):
     xdata.append(frame)
     ydata.append(y)
     zdata.append(z)
-    ln.set_data([xdata,xdata],[ydata,zdata])        
-    # ydata.append(yfunk(frame))
-    # ln.set_data(xdata, ydata)
+    ln.set_data([xdata,xdata],[zdata,ydata])        
     return ln
 
-ani = animation.FuncAnimation(fig, update, frames=np.linspace(0, tmax, 25),
-                    init_func=init,interval=20, repeat=False,blit=False)
+ani = animation.FuncAnimation(fig, update, frames=np.linspace(0, tmax, 500),
+                    init_func=init,interval=10 )
 # repeat_delay=1000,
 
 # To save the animation, use e.g.
-#
 # ani.save("movie.mp4")
 #
 # or
 #
-# from matplotlib.animation import FFMpegWriter
-# writer = FFMpegWriter(fps=15, metadata=dict(artist='Me'), bitrate=1800)
-# ani.save("movie.mp4", writer=writer)
+from matplotlib.animation import FFMpegWriter
+writer = FFMpegWriter(fps=60, metadata=dict(artist='Me'), bitrate=1800)
+ani.save("exp_movie.mp4", writer=writer)
 
 plt.show()
