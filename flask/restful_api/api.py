21c11291905a002c1868f4fd65ffc6ad05b4746a
gunnar pope <gunnar@gmail.com>
2019-08-06 21:34:07 -0400

    making api generic

--- flask/restful_api/api.py
+++ flask/restful_api/api.py
@@ -1,3 +1,6 @@
+"""	api.py
+	an mirror for Henrik's API
+"""
 
 import json
 from flask import Flask, request
@@ -8,6 +11,7 @@ api = Api(app)
 
 data = {'username': 'alice', 'password': 'password1234'}
 data2 = {'username': 'bob', 'password': 'password1234'}
+
 db 	  = {'user': {
 			'id':{
 				1 : data,
@@ -15,20 +19,17 @@ db 	  = {'user': {
 				}
 		}
 
-# API get format
-# GET /version/<resource>/<id>
-class get_userid(Resource):
+
+class resource_api(Resource):
 	def get(self, resource, user_id):
-		print('resource is: ', resource)
-		return {user_id: db[str(resource)]['id'][user_id]}
+		return {user_id: db[resource]['id'][user_id]}
 
 	def post(self, resource, user_id):
-		print('resource is: ', resource)
 		db[resource]['id'][user_id] = request.form
 		return {user_id: db[resource]['id'][user_id]}
 
 
-api.add_resource(get_userid, '/v1/<string:resource>/<int:user_id>')
+api.add_resource(resource_api, '/v1/<string:resource>/<int:user_id>')
 
 
 if __name__ == '__main__':
